31f73c29af579b7c5c5af33dbbe172be8487a53d
ducnp <phuduccva@gmail.com>
2023-06-09 07:43:22 +0700

    my app

---
+++ api/index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+const multer = require("multer");
+const path = require("path")
+
+dotenv.config();
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connect to MongoDB");
+  }
+);
+
+// middleware
+app.use(express.json({limit: '50mb'}));
+app.use(helmet());
+app.use(morgan("common"));
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+// Route example
+// app.get("/",(req, res)=>{
+//     res.send("Welcome to Homepage")
+// })
+// app.get("/users",(req, res)=>{
+//     res.send("Welcome to User page")
+// })
+app.use(express.urlencoded({limit: '50mb'}));
+app.use("images", express.static(path.join(__dirname, "/images")))
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
---
+++ api/routes/auth.js
@@ -0,0 +1,45 @@
+const auth = require("express").Router()
+const User = require("../models/User")
+const bcrypt = require("bcrypt")
+
+
+// REGISTER
+auth.post("/register", async (req, res)=>{     
+   try{
+    //    Generate new password
+       const salt = await bcrypt.genSalt(10)
+       const hashedPassword = await bcrypt.hash(req.body.password, salt)
+    //    Create new user
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword,
+    })
+    // Save user and response
+       const user= await newUser.save()
+       res.status(200).json(user)
+   }catch(err){
+       res.status(500).json(err)
+   }
+}) 
+
+
+// LOGIN
+auth.post("/login", async (req,res)=>{
+    try{
+        // Invalid email and password
+        const user = await User.findOne({email: req.body.email});
+        !user && res.status(404).json("User not found");
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password)
+        !validPassword && res.status(400).json("Wrong password")
+
+        // Valid email and password
+        res.status(200).json(user)
+    }catch(err){
+        res.status(500).json(err)
+    }
+
+})
+
+module.exports =auth ;
