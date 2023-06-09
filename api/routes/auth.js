5efa9260bec785686693779eea2fddafe9e96b60
ducnp <phuduccva@gmail.com>
2023-06-09 20:37:09 +0700

    hash password

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -35,7 +35,8 @@ auth.post("/login", async (req,res)=>{
         !validPassword && res.status(400).json("Wrong password")
 
         // Valid email and password
-        res.status(200).json(user)
+        const {password, ...info} = user._doc
+        res.status(200).json(info)
     }catch(err){
         res.status(500).json(err)
     }
